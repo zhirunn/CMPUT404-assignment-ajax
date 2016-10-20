80136c0e24e1778bfa0cedb0275bdcfdf7354dae
zhirunn <zhirunn@gmail.com>
2016-10-19 23:33:19 -0600

    licensing

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2013 Abram Hindle, Justin Wong
+# Copyright 2016 Abram Hindle, Justin Wong
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
